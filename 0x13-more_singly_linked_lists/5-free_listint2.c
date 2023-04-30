fdd778a9dcdbf9acdd31f3ed24b93c3a46eebb4f
wadiiAnr <wadii.anr@gmail.com>
2023-04-30 11:54:59 -0700

    free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  *free_listint2 - function that frees a listint list
+  *@head: pointer to the listint_t list to be freed.
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+	*head = NULL;
+}
+
