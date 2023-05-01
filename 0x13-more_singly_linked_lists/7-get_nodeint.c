523d223850305696a0b9b7443deb1206c5fa0bb7
wadiiAnr <wadii.anr@gmail.com>
2023-05-01 05:23:49 -0700

    return the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *current = head;
 
-	while (head && i < index)
+	while (current && i < index)
 	{
 		current = current->next;
 		i++;
